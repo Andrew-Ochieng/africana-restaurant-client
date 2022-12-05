6f262ec1f9a67996464a5c1f6b92fdb7884eae2f
Andrew-Ochieng <andrewomosh44@gmail.com>
2022-12-05 21:26:14 +0300

    add order process section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,10 @@
+import OrderProcess from "../components/OrderProcess";
+
 const Home = () => {
     return ( 
         <>
             <div>
-                <h1 className="text-red-500 font-medium text-lg">
-                    Home page coming soon...
-                </h1>
+                <OrderProcess />
             </div>
         </>
      );
