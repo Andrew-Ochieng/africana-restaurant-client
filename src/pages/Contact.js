22059126dbc97c3e2ce7608ed0eb3c217b764fad
Andrew-Ochieng <andrewomosh44@gmail.com>
2022-12-09 01:49:39 +0300

    add footer color

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -30,7 +30,7 @@ const Contact = () => {
                 <div className="bg-green-50 flex justify-center items-center">
                     <div className="md:my-48 my-24 mx-8 md:flex justify-between md:space-x-8 rounded-lg p-8 bg-white">
                         <div>
-                            <h2 className="md:text-xl text-lg font-semibold text-gray-400">Contact Information</h2>
+                            <h2 className="md:text-xl text-lg font-semibold text-gray-500">Contact Information</h2>
                             <div>
                                 <div className="flex my-8">
                                     <FiHome className="text-5xl text-green-500" />
@@ -67,7 +67,7 @@ const Contact = () => {
                         </div>
 
                         <div>
-                            <h2 className="md:text-xl text-lg font-semibold text-gray-400">Send Us A Message</h2>
+                            <h2 className="md:text-xl text-lg font-semibold text-gray-500">Send Us A Message</h2>
                             <form action="#">
                                 <div className="my-4">
                                     <input className="border-2 border-gray-400 rounded-md py-2 px-4 my-2 w-full" type="text" placeholder="Enter Name..."/>
@@ -77,7 +77,7 @@ const Contact = () => {
                                     <input className="border-2 border-gray-400 rounded-md py-2 px-4 w-full" type="text" name="subject" placeholder="Enter Subject..." />
                                 </div>
                                 <textarea className="border-2 border-gray-400 rounded-md py-2 px-4 my-2 w-full" name="message" cols="30" rows="4" placeholder="Enter Message"></textarea>
-                                <button className="py-2 px-4 rounded-md uppercase font-light bg-green-500 ">
+                                <button className="py-2 px-4 rounded-md uppercase font-light bg-green-500 w-full">
                                     Submit Message
                                 </button>
                             </form>
